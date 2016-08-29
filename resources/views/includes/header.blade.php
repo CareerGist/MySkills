ac8f220bac02b0cc8ed1513993ae74fa59f899ab
l4christ <ademolaemmanuel12@yahoo.com>
2016-08-30 00:01:03 +0100

    Add files via upload

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -51,7 +51,7 @@
           </ul> -->
           <form class="navbar-form navbar-left" role="search">
         <div class="form-group input-group">
-          <input type="text" class="form-control" placeholder="Search Skills" style="width:600px;">
+          <input type="text" class="form-control" placeholder="Search Skills">
           <div class="input-group-btn">
             <button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
           </div>
